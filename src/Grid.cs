3152dc1925e7d0cf74b194a13b5a4d83de3275d8
Benjamin Chambers <benjamindchambers@outlook.com>
2015-06-25 11:05:28 -0600

    Initial Commit

---
+++ src/Grid.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSharp
+{
+	public partial class Grid
+	{
+		public Grid() { }
+		public Grid(Grid other)
+		{
+			Array.Copy(other.data, this.data, 81);
+		}
+
+		public int GetCell(Location where) { return data[where.Index]; }
+		public void PutCell(Location where, int value) { data[where.Index] = value; }
+
+		public int[] GetRow(int Row)
+		{
+			int[] result = new int[9];
+
+			Array.Copy(data, Row * 9, result, 0, 9);
+
+			return result;
+		}
+		public int[] GetColumn(int Column)
+		{
+			int[] result = new int[9];
+			int idx = Column;
+			for (int i = 0; i < 9; i++)
+			{
+				result[i] = data[idx];
+				idx += 9;
+			}
+			return result;
+		}
+		public int[] GetZone(int Zone)
+		{
+			int[] result = new int[9];
+			Array.Copy(data, ZoneIndices[Zone], result, 0, 3);
+			Array.Copy(data, ZoneIndices[Zone] + 9, result, 3, 3);
+			Array.Copy(data, ZoneIndices[Zone] + 18, result, 6, 3);
+
+			return result;
+		}
+		public List<int> GetCandidates(Location where)
+		{
+			List<int> result = new List<int>();
+			if (data[where.Index] > 0)
+				return result;
+
+			bool[] blocking = new bool[10];
+			int[] blockingIndices = where.GetConflictingIndices();
+
+			foreach (int idx in blockingIndices)
+			{
+				blocking[data[idx]] = true;
+			}
+
+			for (int i = 1; i < 10; i++)
+				if (!blocking[i])
+					result.Add(i);
+
+			return result;
+		}
+		public static bool AreDistinct(Grid first, Grid second)
+		{
+			for (int i = 0; i < 81; i++)
+				if (!(first.data[i] == second.data[i]))
+					return true;
+			return false;
+		}
+
+		public override string ToString()
+		{
+			string result = data[0].ToString();
+			for (int i = 1; i < 81; i++)
+			{
+				if (i % 27 == 0)
+					result += "\n\n";
+				else
+					if (i % 9 == 0)
+						result += "\n";
+					else
+						if (i % 3 == 0)
+							result += " ";
+
+				result += data[i];
+			}
+
+			return result;
+		}
+
+		private int[] data = new int[81];
+
+		private static int[] ZoneIndices = { 0, 3, 6, 27, 30, 33, 54, 57, 60 };
+	}
+}
---
+++ src/Grid_Fill.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSharp
+{
+	public partial class Grid
+	{
+		public class CouldNotFillException : Exception
+		{
+			public CouldNotFillException() : base() { }
+			public CouldNotFillException(string Message = "") : base(Message) { }
+		}
+
+		public void BruteForceFill()
+		{
+			if (DuplicateValuesPresent)
+				throw new CouldNotFillException("Puzzle has duplicates, could not fill.");
+
+			if (!BruteForceRecursion(0))
+				throw new CouldNotFillException("No solution found.");
+		}
+
+		private bool BruteForceRecursion(int Index)
+		{
+			if (Index == 81)
+				return true;
+
+			if (data[Index] > 0)
+				return BruteForceRecursion(Index + 1);
+			else
+			{
+				List<int> Candidates = GetCandidates((Location)Index);
+
+				foreach (int test in Candidates)
+				{
+					data[Index]=test;
+					if (BruteForceRecursion(Index + 1))
+						return true;
+				}
+				data[Index]=0;
+
+				return false;
+			}
+		}
+
+		public void RandomFill(int Seed)
+		{
+			Random rnd = new Random(Seed);
+
+			if (DuplicateValuesPresent)
+				throw new CouldNotFillException("Puzzle has duplicates, could not fill.");
+
+			if (!RandomRecursion(rnd, 0))
+				throw new CouldNotFillException("No solution found.");
+		}
+
+		private bool RandomRecursion(Random stream, int Index)
+		{
+			if (Index == 81)
+				return true;
+
+			if (data[Index] > 0)
+				return BruteForceRecursion(Index + 1);
+			else
+			{
+				List<int> UnsortedCandidates = GetCandidates((Location)Index);
+				List<int> Candidates = new List<int>();
+
+				foreach (int digit in UnsortedCandidates)
+					Candidates.Insert(stream.Next(Candidates.Count), digit);
+
+				foreach (int test in Candidates)
+				{
+					data[Index] = test;
+					if (BruteForceRecursion(Index + 1))
+						return true;
+				}
+				data[Index] = 0;
+
+				return false;
+			}
+		}
+	}
+}
+
