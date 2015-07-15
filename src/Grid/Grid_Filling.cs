b8d4924a456388870d213105229c7f7ce831b755
Benjamin Chambers <benjamindchambers@outlook.com>
2015-07-15 12:46:45 -0600

    Minor cleanup

--- src/Grid/Grid_Filling.cs
+++ src/Grid/Grid_Filling.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SudokuSharp
+﻿namespace SudokuSharp
 {
     public partial class Grid
     {
