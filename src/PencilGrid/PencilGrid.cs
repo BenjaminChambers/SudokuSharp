ed65792d9ed2f1314bbd19d5983d91d8d5776a3d
Benjamin Chambers <benjamindchambers@outlook.com>
2015-07-19 19:48:40 -0600

    Doc updates.

--- src/PencilGrid/PencilGrid.cs
+++ src/PencilGrid/PencilGrid.cs
@@ -3,6 +3,10 @@ using System.Runtime.Serialization;
 
 namespace SudokuSharp
 {
+    /// <summary>
+    /// A grid for keeping track of all the pencil marks.
+    /// Many players will mark in a cell the values that may be placed there; this is represented by an array of <see cref="bool"/> values for every <see cref="Location"/> on the board.
+    /// </summary>
     [DataContract]
     public class PencilGrid
     {
@@ -31,11 +35,42 @@ namespace SudokuSharp
         }
         #endregion
 
+        /// <summary>
+        /// Returns whether a pencil mark is present.
+        /// </summary>
+        /// <param name="where">The <see cref="Location"/> to check.</param>
+        /// <param name="value">The number to check.</param>
+        /// <returns></returns>
         public bool Get(Location where, int value) { return data[where][value]; }
+        /// <summary>
+        /// Sets a pencil mark.
+        /// </summary>
+        /// <param name="where">The <see cref="Location"/> to set.</param>
+        /// <param name="value">The number to set.</param>
+        /// <returns></returns>
         public bool Set(Location where, int value) { return data[where][value] = true; }
+        /// <summary>
+        /// Clears the specified pencil mark.
+        /// </summary>
+        /// <param name="where">The <see cref="Location"/> to clear.</param>
+        /// <param name="value">The number to clear.</param>
+        /// <returns></returns>
         public bool Clear(Location where, int value) { return data[where][value] = false; }
+        /// <summary>
+        /// Toggles the specified pencil mark.
+        /// </summary>
+        /// <param name="where">The <see cref="Location"/> to toggle.</param>
+        /// <param name="value">The number to toggle.</param>
+        /// <returns></returns>
         public bool Toggle(Location where, int value) { return (data[where][value] = !data[where][value]); }
 
+        /// <summary>
+        /// Returns an array of all the pencil marks at a given <see cref="Location"/>.
+        /// It is an array of 10 booleans, so you may access it using 1-9 for the given cell values.
+        /// (I know, it doesn't make sense to have a pencil mark for 0, just ignore that index)
+        /// </summary>
+        /// <param name="where">The <see cref="Location"/>.</param>
+        /// <returns></returns>
         public bool[] GetMarks(Location where)
         {
             bool[] result = new bool[10];
