2f165857bd29c217ac9b0005c96dbcb7a2871825
Benjamin Chambers <benjamindchambers@outlook.com>
2015-07-19 19:59:45 -0600

    Doc updates

--- src/History/PencilMarks.cs
+++ src/History/PencilMarks.cs
@@ -2,49 +2,28 @@
 
 namespace SudokuSharp.History
 {
-    /// <summary>
-    /// The base class for PencilMark history items. It contains logic useful for all three.
-    /// </summary>
     [DataContract]
-    public abstract class PencilActionBase
+    internal abstract class PencilActionBase
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PencilActionBase"/> class.
-        /// </summary>
-        /// <param name="Target">The <see cref="PencilGrid"/> being marked.</param>
-        /// <param name="Where">The <see cref="Location"/> of the mark.</param>
-        /// <param name="Value">The number being marked.</param>
-        public PencilActionBase(PencilGrid Target, Location Where, int Value)
+        internal PencilActionBase(PencilGrid Target, Location Where, int Value)
         {
             _target = Target;
             _where = Where;
             _value = Value;
         }
-
-        /// <summary>
-        /// Internal. Do not reference.
-        /// </summary>
+        
         [DataMember]
         protected PencilGrid _target;
-        /// <summary>
-        /// Internal. Do not reference.
-        /// </summary>
         [DataMember]
         protected Location _where;
-        /// <summary>
-        /// Internal. Do not reference.
-        /// </summary>
         [DataMember]
         protected int _value;
-        /// <summary>
-        /// Internal. Do not reference.
-        /// </summary>
         [DataMember]
         protected bool _prior;
     }
-
+    
     [DataContract]
-    public class PencilToggle : PencilActionBase, IHistoryAction
+    internal class PencilToggle : PencilActionBase, IHistoryAction
     {
         public PencilToggle(PencilGrid Target, Location Where, int Value) : base(Target, Where, Value) { }
         public void Apply() { _target.Toggle(_where, _value); }
@@ -52,7 +31,7 @@ namespace SudokuSharp.History
     }
 
     [DataContract]
-    public class PencilSet : PencilActionBase, IHistoryAction
+    internal class PencilSet : PencilActionBase, IHistoryAction
     {
         public PencilSet(PencilGrid Target, Location Where, int Value) : base(Target, Where, Value) { }
         public void Apply()
@@ -67,7 +46,7 @@ namespace SudokuSharp.History
     }
 
     [DataContract]
-    public class PencilClear : PencilActionBase, IHistoryAction
+    internal class PencilClear : PencilActionBase, IHistoryAction
     {
         public PencilClear(PencilGrid Target, Location Where, int Value) : base(Target, Where, Value) { }
         public void Apply()
