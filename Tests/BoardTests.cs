4b3641ad263d08f03b70bfa70ab1f281b67557e3
Benjamin Chambers <benjamindchambers@outlook.com>
2015-09-18 15:28:15 -0600

    Began adding unit tests

---
+++ Tests/BoardTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        static int Iterations = 0;
+
+        static List<SudokuSharp.Board> Work = new List<SudokuSharp.Board>();
+
+        [ClassInitialize]
+        public static void BoardTestsInitialize(TestContext context)
+        {
+            Iterations = int.Parse(context.Properties["Iterations"].ToString());
+        }
+
+        [TestMethod]
+        public void CreateSolution()
+        {
+            for (int i = 0; i < Iterations; i++)
+                Work.Add(SudokuSharp.Board.CreateSolution(i));
+        }
+
+        [TestMethod]
+        public void CreatePuzzle()
+        {
+            for (int i = 0; i < Iterations; i++)
+                SudokuSharp.Board.CreatePuzzle(Work[i], i, 30, 4);
+        }
+    }
+}
