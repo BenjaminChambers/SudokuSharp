ae22a1a78d90d8616d3891a77b0a665dfc5df2e8
Benjamin Chambers <benjamindchambers@outlook.com>
2015-12-29 13:39:37 -0700

    Added stub for FindLockedCandidates

---
+++ src/Board/Find/LockedCandidates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SudokuSharp
+{
+    public partial class Board
+    {
+        /// <summary>
+        /// For every zone, checks to see if a candidate is confined to only one row or column.
+        /// If it is, then it checks for naked or hidden singles after removing that row or column as a possibility from other zones
+        /// </summary>
+        /// <returns><see cref="Dictionary{Location, Int}"/></returns>
+        public Dictionary<Location, int> FindLockedCandidates()
+        {
+        }
+    }
+}
