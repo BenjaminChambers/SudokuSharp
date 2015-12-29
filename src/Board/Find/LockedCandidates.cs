dd81ba91c46bf05368d35ba19ff81eb029b3b353
Benjamin Chambers <benjamindchambers@outlook.com>
2015-12-29 14:00:18 -0700

    Fleshed out most of LockedCandidates

--- src/Board/Find/LockedCandidates.cs
+++ src/Board/Find/LockedCandidates.cs
@@ -14,6 +14,51 @@ namespace SudokuSharp
         /// <returns><see cref="Dictionary{Location, Int}"/></returns>
         public Dictionary<Location, int> FindLockedCandidates()
         {
+            var candidates = FindCandidatesForAllEmptyCells();
+
+            for (int number = 1; number<9; number++)
+            {
+                var numberLocations = from item in candidates
+                                      where item.Value.Contains(number)
+                                      select item.Key;
+
+                for (int zone=0; zone<9; zone++)
+                {
+                    var numberLocationsInThisZone = from loc in numberLocations
+                                                    where loc.Zone == zone
+                                                    select loc;
+
+                    int lockedRow = numberLocationsInThisZone.First().Row;
+                    int lockedColumn = numberLocationsInThisZone.First().Column;
+
+                    foreach (var loc in numberLocationsInThisZone)
+                    {
+                        if (loc.Row != lockedRow) lockedRow = -1;
+                        if (loc.Column != lockedColumn) lockedColumn = -1;
+                    }
+
+                    if (lockedRow != -1)
+                    {
+                        foreach (var loc in
+                            (from item in numberLocations
+                             where item.Zone != zone
+                             where item.Row == lockedRow
+                             select item))
+                            candidates[loc].Remove(number);
+                    }
+                    if (lockedColumn != -1)
+                    {
+                        foreach (var loc in
+                            (from item in numberLocations
+                             where item.Zone != zone
+                             where item.Column == lockedColumn
+                             select item))
+                            candidates[loc].Remove(number);
+                    }
+                }
+            }
+
+//            var results = FindNakedSingles(candidates);
         }
     }
 }
