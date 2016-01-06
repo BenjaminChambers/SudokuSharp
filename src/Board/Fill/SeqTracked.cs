26c750b5a74500fe35d2ffeca08ddc9b151205bd
Benjamin Chambers <benjamindchambers@outlook.com>
2016-01-06 15:21:22 -0700

    Further refinements to SeqTracked

--- src/Board/Fill/SeqTracked.cs
+++ src/Board/Fill/SeqTracked.cs
@@ -7,68 +7,85 @@ namespace SudokuSharp
     {
         public partial class _FillClass
         {
-            int[,] _numInRow = new int[10, 9];
-            int[,] _numInColumn = new int[10, 9];
-            int[,] _numInZone = new int[10, 9];
+            private class FillRecursionHelper
+            {
+                public FillRecursionHelper(Board src)
+                {
+                    _work = new Board(src);
+
+                    foreach (var loc in Location.All)
+                    {
+                        _numInRow[_work[loc], loc.Row]++;
+                        _numInColumn[_work[loc], loc.Column]++;
+                        _numInZone[_work[loc], loc.Zone]++;
+                    }
+                }
+
+                public static explicit operator Board(FillRecursionHelper h)
+                {
+                    return h._work;
+                }
+
+                public int this[Location Where]
+                {
+                    get { return _work[Where]; }
+                    set
+                    {
+                        int old = _work[Where];
+                        _work[Where] = value;
+
+                        _numInRow[old, Where.Row]--;
+                        _numInColumn[old, Where.Column]--;
+                        _numInZone[old, Where.Zone]--;
+                        _numInRow[value, Where.Row]++;
+                        _numInColumn[value, Where.Column]++;
+                        _numInZone[value, Where.Zone]++;
+                    }
+                }
+
+                public IEnumerable<int> Candidates(Location loc)
+                {
+                    return from i in new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+                           where _numInRow[i, loc.Row] == 0
+                           where _numInColumn[i, loc.Column] == 0
+                           where _numInZone[i, loc.Zone] == 0
+                           select i;
+                }
+
+                int[,] _numInRow = new int[10, 9];
+                int[,] _numInColumn = new int[10, 9];
+                int[,] _numInZone = new int[10, 9];
+                Board _work;
+            }
             Board _work;
 
             public Board SeqTracked()
             {
-                _work = new Board(_parent);
-                _numInRow = new int[10, 9];
-                _numInColumn = new int[10, 9];
-                _numInZone = new int[10, 9];
+                FillRecursionHelper work = new FillRecursionHelper(_parent);
 
-                foreach (var loc in Location.All)
-                {
-                    _numInRow[_work[loc], loc.Row]++;
-                    _numInColumn[_work[loc], loc.Column]++;
-                    _numInZone[_work[loc], loc.Zone]++;
-                }
-
-                if (BruteForceTracked(0))
-                    return _work;
+                if (BruteForceTracked(work, 0))
+                    return (Board)work;
 
                 return null;
             }
 
-            IEnumerable<int> Possible(Location loc)
-            {
-                return from i in new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
-                       where _numInRow[i, loc.Row] == 0
-                       where _numInColumn[i, loc.Column] == 0
-                       where _numInZone[i, loc.Zone] == 0
-                       select i;
-            }
-
-            private void ChangeNumber(Location loc, int value)
-            {
-                int remove = _work[loc];
-                _numInRow[remove, loc.Row]--;
-                _numInColumn[remove, loc.Column]--;
-                _numInZone[remove, loc.Zone]--;
-                _work[loc] = value;
-                _numInRow[value, loc.Row]++;
-                _numInColumn[value, loc.Column]++;
-                _numInZone[value, loc.Zone]++;
-            }
-
-            private bool BruteForceTracked(int Index)
+            private bool BruteForceTracked(FillRecursionHelper work, int Index)
             {
                 if (Index == 81)
                     return true;
 
-                if (_work[Index] > 0)
-                    return BruteForceTracked(Index + 1);
+                if (work[Index] > 0)
+                    return BruteForceTracked(work, Index + 1);
                 else
                 {
-                    foreach (int test in Possible(Index))
+                    foreach (int test in work.Candidates(Index))
                     {
-                        ChangeNumber(Index, test);
+                        work[Index] = test;
 
-                        if (BruteForceTracked(Index + 1))
+                        if (BruteForceTracked(work, Index + 1))
                             return true;
                     }
+                    work[Index] = 0;
 
                     return false;
                 }
