5b28ae97d3e7baae3b2fadb9182c365fdbdef7a4
Benjamin Chambers <benjamindchambers@outlook.com>
2019-09-17 15:11:20 -0600

    Initial tests

---
+++ Tests/Singles.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static SudokuSharp.Board t1 = new SudokuSharp.Board();
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
