b8baf088982f0c8d0eb33f40269a3848eb29e664
Benjamin Chambers <benjamindchambers@outlook.com>
2019-09-30 17:35:39 -0600

    Updated timing projects to .Net Core 3.0

---
+++ TimePuzzleGeneration/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TimePuzzleGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random(0);
+            var brd = SudokuSharp.Factory.Solution(rnd);
+
+            int BatchSize = 10;
+
+            var results = new List<(int Batch, double Seconds)>();
+
+            TimeSpan Elapsed;
+            do
+            {
+                BatchSize = (3 * BatchSize) / 2;
+                var start = DateTime.Now;
+                for (int i = 0; i < BatchSize; i++)
+                    SudokuSharp.Factory.Puzzle(brd, rnd, 10, 10, 10);
+                Elapsed = DateTime.Now - start;
+                Console.WriteLine($"{BatchSize} Puzzles created in {Elapsed.TotalSeconds} seconds.");
+                results.Add((BatchSize, Elapsed.TotalSeconds));
+            } while (Elapsed.TotalSeconds < 3.0f);
+
+            Console.WriteLine();
+            Console.WriteLine($"Puzzles per second: {results.Sum(x => x.Batch) / results.Sum(x => x.Seconds)}");
+        }
+    }
+}
---
+++ TimeSolutionGeneration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TimeSolutionGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random(0);
+
+            int BatchSize = 1_000;
+
+            var results = new List<(int Batch, double Seconds)>();
+
+            TimeSpan Elapsed;
+            do
+            {
+                BatchSize = (3 * BatchSize) / 2;
+                var start = DateTime.Now;
+                for (int i = 0; i < BatchSize; i++)
+                    SudokuSharp.Factory.Solution(rnd);
+                Elapsed = DateTime.Now - start;
+                Console.WriteLine($"{BatchSize} Solutions created in {Elapsed.TotalSeconds} seconds.");
+                results.Add((BatchSize, Elapsed.TotalSeconds));
+            } while (Elapsed.TotalSeconds < 3.0f);
+
+            Console.WriteLine();
+            Console.WriteLine($"Solutions per second: {results.Sum(x => x.Batch) / results.Sum(x => x.Seconds)}");
+        }
+    }
+}
